14406b42a828fb9292772b0e0196f01cad09d7a2
Sergio Cordero <sergio.crol@gmail.com>
2023-08-27 12:34:50 +0200

    Change image protocol to accept all

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.stablediffusionapi.com",
-        port: "",
-        pathname: "/**",
+        hostname: "**",
       },
     ],
   },
